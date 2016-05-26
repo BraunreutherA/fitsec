a28181334f1a9025a25c993d4c134e0ce9a4152e
pranges <pranges@cip.ifi.lmu.de>
2016-05-26 23:02:24 +0200

    added switch instead of toggle button

--- app/src/main/java/secureapps/com/fitsec/SecureAppAdapter.java
+++ app/src/main/java/secureapps/com/fitsec/SecureAppAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -87,7 +88,7 @@ public class SecureAppAdapter extends RecyclerView.Adapter<SecureAppAdapter.Secu
         TextView secureCount;
 
         @BindView(R.id.app_secure_toggle)
-        ToggleButton toggle;
+        Switch toggle;
 
         public SecureAppViewHolder(View itemView) {
             super(itemView);
