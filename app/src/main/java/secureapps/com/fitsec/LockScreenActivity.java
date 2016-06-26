0cf8a36477cced627ed348fe96b681265143e617
pranges <pranges@cip.ifi.lmu.de>
2016-06-26 16:33:17 +0200

    tried lock screen stuff

---
+++ app/src/main/java/secureapps/com/fitsec/LockScreenActivity.java
@@ -0,0 +1,78 @@
+package secureapps.com.fitsec;
+
+import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by Sarah on 26.06.2016.
+ */
+public class LockScreenActivity extends Activity implements OnClickListener {
+
+    private static final int ADMIN_INTENT = 15;
+    private static final String description = "Sample Description";
+    private DevicePolicyManager devicePolicyManager;
+    private ComponentName componentName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_lock);
+        devicePolicyManager = (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
+        componentName = new ComponentName(this, MyAdminReceiver.class);
+
+        Button btnEnableAdmin = (Button) findViewById(R.id.btnEnable);
+        Button btnDisableAdmin = (Button) findViewById(R.id.btnDisable);
+        Button btnLock = (Button) findViewById(R.id.btnLock);
+        btnEnableAdmin.setOnClickListener(this);
+        btnDisableAdmin.setOnClickListener(this);
+        btnLock.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnEnable:
+                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
+                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,description);
+                startActivityForResult(intent, ADMIN_INTENT);
+                break;
+
+            case R.id.btnDisable:
+                devicePolicyManager.removeActiveAdmin(componentName);
+                Toast.makeText(getApplicationContext(), "Admin registration removed", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btnLock:
+                boolean isAdmin = devicePolicyManager.isAdminActive(componentName);
+                if (isAdmin) {
+                    devicePolicyManager.lockNow();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Not Registered as admin", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ADMIN_INTENT) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Registered As Admin", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "Failed to register as Admin", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/secureapps/com/fitsec/MyAdminReceiver.java
@@ -0,0 +1,10 @@
+package secureapps.com.fitsec;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by Sarah on 26.06.2016.
+ */
+public class MyAdminReceiver extends DeviceAdminReceiver{
+
+}
